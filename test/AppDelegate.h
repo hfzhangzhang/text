5131f586549a92513ac28806fd92327c186fe77b
hfzhangzhang <1216110854@qq.com>
2018-05-01 14:58:35 +0800

    一次提交

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by hfzhangzhang on 2018/5/1.
+//  Copyright © 2018年 hfzhangzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
